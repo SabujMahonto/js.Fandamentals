bdb315d847f59611e674ac0ac4b6dc2efa847e7e
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-27 21:51:09 +0600

    fandamantal.js start

---
+++ variable.js
@@ -0,0 +1,16 @@
+// Number 
+var eggPrice = 20;
+ eggPrice = 22;
+
+//  String 
+var userName = 'Jon Duo'
+ 
+// Boolean 
+var isExpensive = true;
+
+
+var mangoPrice = 20.50;
+var bananaPrice = 30;
+var totalPrice = mangoPrice + bananaPrice;
+
+parseFloat()
