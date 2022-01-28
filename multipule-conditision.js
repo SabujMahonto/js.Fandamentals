46b117cd741d78c76f50f683721ca3c80cb4bca6
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 18:25:39 +0600

    multipule conditision done

---
+++ multipule-conditision.js
@@ -0,0 +1,18 @@
+var gotJob = true
+monySave = 20000;
+var hasFlat = false
+
+
+// if( gotJob == true && hasFlat == true){
+//     console.log('You can got marriage');
+// }
+// else{
+//     console.log('You are not fit');
+// }
+
+if( gotJob == true || hasFlat == true){
+    console.log('You can got marriage');
+}
+else{
+    console.log('You are not fit');
+}
