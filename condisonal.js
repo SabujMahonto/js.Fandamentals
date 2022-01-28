b4aee2368886c2b1fcdf400d4175864f704f1737
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 17:47:04 +0600

    condisonal concept done

---
+++ condisonal.js
@@ -0,0 +1,16 @@
+var chickenPrice = 220;
+var myMoney = 500;
+
+// if(chickenPrice > myMoney){
+//     console.log('!Yes , I eat chicken.');
+// }
+// else{
+//     console.log('I eat fish.');
+// }
+
+if(chickenPrice < myMoney){
+    console.log('!Yes , I eat chicken.');
+}
+else{
+    console.log('I eat fish.');
+}
