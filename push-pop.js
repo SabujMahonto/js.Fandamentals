f023c54570a8584e99a099ce94efd8d8b2e0cdd9
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 15:46:03 +0600

    array method learning done

---
+++ push-pop.js
@@ -0,0 +1,25 @@
+var friendsList = [ 'sohohag', 'sojol', 'otol', 'rony', 'shakib'];
+
+//  Array add Element in th end
+friendsList.push('sabuj');
+console.log(friendsList);
+
+var friendsAge = [11, 13, 14, 15,16];
+ friendsAge.push(88);
+ console.log(friendsAge);
+
+//  Array add Element in th first 
+friendsList.unshift('somir')
+friendsAge.unshift(16)
+
+
+//  remove an elements an array  in the end
+ friendsList.pop()
+ console.log(friendsList);
+
+
+//  console.log(friendsAge);
+
+
+
+
