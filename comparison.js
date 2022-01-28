6c167408f6ee9a5d49e1afbfa48c1666b6c848b7
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 17:29:53 +0600

    comparison operator learning done

---
+++ comparison.js
@@ -0,0 +1,16 @@
+console.log(5 > 6);      //lese than
+console.log(5 < 6);      //geterr than
+console.log(5 == 6);     // duboll equal 
+console.log(6 == 6);     //equal to 
+console.log(6 != 6);     //alada alada not equal  kina
+
+var myLove = 99;
+var yourLove = 100;
+
+console.log(myLove > yourLove);
+console.log(myLove < yourLove);
+console.log(myLove == yourLove);
+console.log(myLove != yourLove);
+
+// console.log(goodStudent && goodResult); //&&
+// console.log(isBanker && goodPerson); //||
