5f430af16d420488538f0e2b0e0fc6193834b7ff
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 22:02:29 +0600

    nasted condisition clear

---
+++ nasted-condition .js
@@ -0,0 +1,15 @@
+var mangoPrice = 280;
+var orangePrice = 250;
+var applePrice = 300;
+var bananaPrice = 50;
+
+
+if(mangoPrice > applePrice){
+    console.log('you can buy mango');
+}
+else if(mangoPrice < orangePrice){
+    console.log('you can buy mango');
+}
+else{
+    console.log('I can buy banana');
+}
