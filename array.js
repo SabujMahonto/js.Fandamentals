2c9e51c0b24ad9dd43c14aef602005333ae381da
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 12:00:01 +0600

    array consept done

---
+++ array.js
@@ -0,0 +1,14 @@
+var friendsAge = [11, 17 , 20, 33, 15];
+console.log(friendsAge);
+
+var partyFee = [200,100,230,105];
+console.log(partyFee);
+
+var friendNames = ["tim", "tom","kim", "justin"];
+console.log(friendNames);
+
+var oddNumber = [1,3,5,7];
+console.log(oddNumber);
+
+var evenNumber = [2,4,6,8]
+console.log(evenNumber);
