ccc8fce710975486593822e7775b82650a0962cd
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-01-28 12:42:00 +0600

    array indexOf , concept clear

---
+++ array-index.js
@@ -0,0 +1,18 @@
+var bookName = ['hablu' , 'bolod', 'battery', 'parasitamol'];
+
+var batteryIndex = bookName.indexOf('battery');
+console.log(batteryIndex);
+
+
+var numbers = [22, 34,33,55,80,23,];
+var index = numbers.indexOf(55);
+console.log(index);
+
+var secondIndex = numbers.indexOf[3];
+console.log(secondIndex);
+
+// element change korte 
+
+ numbers [1] = 11;
+ numbers [4] = 100;
+ console.log(numbers);
